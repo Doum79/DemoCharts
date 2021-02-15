02ac7215913816463acfaee489b2e535b7dd9421
Doum79 <bayeaziz77@gmail.com>
2021-02-15 11:28:41 +0100

    display

--- DemoCharts/Controllers/HomeController.cs
+++ DemoCharts/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using DemoCharts.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +12,56 @@ namespace DemoCharts.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //public static float SystemCPU { get; private set; }
+       // private static readonly object locker = new object();
+       private static readonly PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", true) ;
+        private static readonly PerformanceCounter meCounter = new PerformanceCounter("Memory", "% Committed Bytes in use");
+         private static readonly PerformanceCounter diskCounter = new PerformanceCounter("LogicalDisk", "% Free Space", "_Total", true);
+        public ActionResult Index(Capacite c)
         {
-            return View();
+            c.CPU = GetCpuValue();
+            c.Memory = GetMemValue();
+            c.Disk = GetDiskValue();
+          
+
+
+            return View(c) ;
         }
+
+        private static string GetCpuValue()
+        {
+
+      var value =   (int)cpuCounter.NextValue();
+            //Note: In most cases you need to call .NextValue() twice to be able to get the real value
+            string val = value.ToString()+ "%";
+            Thread.Sleep(1000);
+            return val;
+        }
+
+
+        private static string GetMemValue()
+        {
+
+            var value = (int)meCounter.NextValue();
+            //Note: In most cases you need to call .NextValue() twice to be able to get the real value
+            string val = value.ToString() + "%";
+            return val;
+        }
+
+
+        private static string GetDiskValue()
+        {
+            var value = (int)diskCounter.NextValue();
+            //Note: In most cases you need to call .NextValue() twice to be able to get the real value
+            string val = value.ToString() + "%";
+            Thread.Sleep(1000);
+            return val;
+        }
+
+
+
+
+
 
         public ActionResult About()
         {
---
+++ DemoCharts/Models/Capacite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoCharts.Models
+{
+    public class Capacite
+    {
+        public string CPU { get; set; } 
+        public string Memory { get; set; }
+        public string Disk { get; set; }
+    }
+}
